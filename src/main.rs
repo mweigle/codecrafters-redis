84b2f7a699b3c867eb71ea10ec70b9719b244e2e
hm-weigle <maximilian.weigle@hm.edu>
2024-03-14 20:30:49 +0100

    switch to async

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,29 @@
-use std::{
-    io::{BufRead, BufReader, Write},
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
 };
 
-fn main() -> anyhow::Result<()> {
-    println!("Logs from your program will appear here!");
-
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                handle_connection(stream)?;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                tokio::spawn(handle_connection(stream));
             }
             Err(e) => {
                 anyhow::bail!("error: {}", e);
             }
         }
     }
-    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
     loop {
         let reader = BufReader::new(&mut stream);
         let mut lines_reader = reader.lines();
-        if let Some(next_line) = lines_reader.next() {
-            let _line = next_line?;
-            write!(stream, "+PONG\r\n")?;
+        if let Some(_next_line) = lines_reader.next_line().await? {
+            stream.write_all(b"+PONG\r\n").await?;
         }
     }
 }
